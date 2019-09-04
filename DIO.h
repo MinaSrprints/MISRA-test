705b529089df39d50c5c55599b5186360afd870c
MinaSrprinter <minagsobhy@gmail.com>
2019-09-04 15:30:16 +0200

    KEYPAD is done

--- DIO.h
+++ DIO.h
@@ -2,7 +2,7 @@
  * DIO.h
  *
  * Created: 24/08/2015 12:51:11 ص
- *  Author: hossam
+ *  Author: mina
  */ 
 
 
@@ -13,14 +13,18 @@
 #include "Basic_Types.h"
 
 
-#define PA 0x00
-#define PB 0x01
-#define PC 0x02
-#define PD 0x03
+#define PA ((unsigned char)0x00)
+#define PB ((unsigned char)0x01)
+#define PC ((unsigned char)0x02)
+#define PD ((unsigned char)0x03)
 
 void DIO_InitPortDirection(u8 PortName,u8 PortDirection,u8 PortMask);
 void DIO_WritePort(u8 PortName,u8 PortData,u8 PortMask);
 u8 DIO_ReadPort(u8 PortName,u8 PortMAsk);
+
+void DIO_vidReadPortData(u8 x,u8 y,unsigned char* z);
+void DIO_vidWritePortData(u8 x,u8 y,unsigned char z);
+void DIO_vidWritePortDirection(u8 x,u8 y,u8 z);
 
 
 
--- KEYPAD.h
+++ KEYPAD.h
@@ -2,7 +2,7 @@
  * KEYPAD.h
  *
  * Created: 30/01/2016 07:01:25 م
- *  Author: hossam
+ *  Author: mina
  */ 
 
 
@@ -10,8 +10,25 @@
 #define KEYPAD_H_
 
 
-void KPD_Init(void);
-void KPD_ReadVal(unsigned char* ValuePtr);
+#define KPD_COL_PORT (PD)
+#define KPD_ROW_PORT (PD)
+#define KPD_COL_MASK ((unsigned char)0x70)
+#define KPD_ROW_MASK ((unsigned char)0x0f)
+#define KPD_COL_PIN_NUM (4u)
+#define KPD_ROW_PIN_NUM (0u)
+#define APS_ZERO ((unsigned char)0x00)
+#define APS_ONE ((unsigned char)0xFF)
+/**************************************************/
 
+
+void KPD_Init(void);
+
+
+void KPD_Init(void);
+void KPD_ReadVal(char* ValuePtr);
+void KPD_COL_READ(unsigned char* VALPTR);
+void KPD_ROW_WRITE(unsigned char DATA);
+void KPD_COL_INIT(void);
+void KPD_ROW_INIT(void);
 
 #endif /* KEYPAD_H_ */
